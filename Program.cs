514ec6882b5f511301cad6a5005b5090c2ec3d6e
Ujjwol Lamichhane <ujjwol.lamichhane@selu.edu>
2014-10-01 16:37:17 -0500

    Reorganize directory for clarity

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MimeSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var MimeSharp = new MimeSharp();
+
+            Console.WriteLine("MimeSharp example usage: ");
+            Console.WriteLine("MIME type for {0} is {1}", "song.ogg", MimeSharp.Lookup(@"song.ogg"));
+            Console.WriteLine("The default MIME type is {0}", MimeSharp.DefaultType());
+            Console.WriteLine("File extensions that match the mime type {0} are :", "audio/ogg");
+            Console.WriteLine(JsonConvert.SerializeObject(MimeSharp.Extension("audio/ogg")));
+        }
+    }
+}
