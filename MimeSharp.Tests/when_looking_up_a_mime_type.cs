f02171f4ee41b58967e80df0117f7d0cf764f554
Ben Lucas <blucas@obsglobal.com>
2016-08-18 09:43:21 -0700

    add unit tests for HTML mime types add unit tests for issue caused when using component more than once

---
+++ MimeSharp.Tests/when_looking_up_a_mime_type.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace MimeSharp.Tests
+{
+    public class when_looking_up_a_mime_type
+    {
+        [Fact]
+        public void html_extension_should_have_correct_mime_type()
+        {
+            var result = Mime.Lookup("test.html");
+            result.Should().Be("text/html");
+        }
+
+        [Fact]
+        public void htm_extension_is_correctly_identified()
+        {
+            var result = Mime.Lookup("test.htm");
+            result.Should().Be("text/html");
+        }
+    }
+}
---
+++ MimeSharp.Tests/when_performing_multiple_mime_lookups.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace MimeSharp.Tests
+{
+    public class when_performing_multiple_mime_lookups
+    {
+        [Fact]
+        public void supports_making_multiple_lookups()
+        {
+            var firstResult = Mime.Lookup("test.html");
+            var secondResult = Mime.Lookup("test.pdf");
+
+            firstResult.Should().NotBeNullOrEmpty();
+            secondResult.Should().NotBeNullOrEmpty();
+        }
+    }
+}
